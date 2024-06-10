1812bff07cf12027fcf22917bafa1b9448759805
Mahesh <mahesh001.mg@gmail.com>
2024-06-10 20:01:42 +0530

    User,Cart,Orders added

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const server = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const { createProduct } = require("./controller/Product");
 const productsRouter = require("./routes/Products");
 const categoriesRouter = require("./routes/Categories");
 const brandsRouter = require("./routes/Brands");
-const cors=require("cors")
-
+const usersRouter = require("./routes/Users");
+const authRouter = require("./routes/Auth");
+const cartRouter = require("./routes/Carts");
+const ordersRouter = require("./routes/Order");
 //middlewares
-server.use(cors({exposedHeaders:['X-Total-Count']}))
+server.use(cors({ exposedHeaders: ["X-Total-Count"] }));
 server.use(express.json());
 server.use("/products", productsRouter.router);
 server.use("/categories", categoriesRouter.router);
 server.use("/brands", brandsRouter.router);
-
+server.use("/users", usersRouter.router);
+server.use("/auth", authRouter.router);
+server.use("/cart", cartRouter.router);
+server.use("/orders", ordersRouter.router);
 main().catch((err) => console.log(err));
 
 async function main() {
@@ -27,3 +34,6 @@ server.get("/", (req, res) => {
 server.listen(8080, () => {
   console.log("server started");
 });
+
+
+
